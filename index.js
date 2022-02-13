da6b028439bc4d67aceea4cbf899c214b0e7dd46
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2022-02-12 22:45:44 -0800

    ended up doing research on front end libraries

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const MidiPlayer = require('midi-player-js');
 
 // Initialize player and register event handler
 const Player = new MidiPlayer.Player(function(event) {
-	console.log(event);
+	console.log(event.noteNumber);
 });
 
 // Load a MIDI file
 Player.loadFile('./src/midi/sheiks-theme.mid');
 Player.play();
-
