ae39c99636d74a28aafd0c70aacb7765cf24d091
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2022-02-27 13:33:28 -0800

    add piano control component

--- react-sequencer/src/App.js
+++ react-sequencer/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import Keys from "./components/Keys";
+import PianoControls from "./components/PianoControls";
 
 export default function App() {
   return (
     <div className="app">
       <Keys></Keys>
+      <PianoControls></PianoControls>
     </div>
   );
 }
